fbc20a85d89817cbb345ad5b0e6da01bfe80df3b
root <root@localhost.localdomain>
2020-07-05 23:02:28 +0400

    [dev] client operationel, serveur operationel(ls,rm)

--- src/main.c
+++ src/main.c
@@ -2,9 +2,20 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-
+#include <string.h>
 
 int main() {
+	FILE *p;
+	char buff[256];
+	p = popen("rm rien","r");
+	
+	for(int i=1;i<5;i++) {
+	bzero(buff,256);
+	fgets(buff,256,p);
 
-return 0;
+	printf("a : %s",buff);
+	}
+	pclose(p);
+	printf("Fin \n");
+	return 0;
 }
